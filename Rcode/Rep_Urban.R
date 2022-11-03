03804046052707bf518791135edc10274a159297
WONKIMWK <wonjongkim.wkim@gmail.com>
2022-11-03 12:07:39 -0500

    test

--- Rcode/Rep_Urban.R
+++ Rcode/Rep_Urban.R
@@ -3,14 +3,14 @@
 # ----------------------------------------------------------------
 # Contact Information--------------------------------
 
-# Modified    : Oct 27 2022
+# Modified    : Nov 02 2022
 # Created     : Oct 25 2022
 # Author      : Wonjong Kim
 # Affiliation : University of Alabama
 # E-mail      : wkim22@crimson.ua.edu
 # ----------------------------------------------------------------------
 
-drct <- paste0(getwd(), "/Data/124621-V1/replicate_monitor/1_build")
+drct <- paste0(getwd(), "/Data/1_build")
 
 epa.2001 <- read.csv(file = paste0(drct, "/epa/raw/annual_all_2001.csv"),
                   header = TRUE) 
